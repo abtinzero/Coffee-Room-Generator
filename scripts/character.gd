eccd86a465465362749f9076e917934b1e975687
abtinzero <abtinsaadat@gmail.com>
2023-05-18 12:12:08 +0200

    add world and character scenes

---
+++ scripts/character.gd
@@ -0,0 +1,12 @@
+extends CharacterBody3D
+
+var gravity = 10
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	velocity.y -= gravity * delta
+	move_and_slide()
